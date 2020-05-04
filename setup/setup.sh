e9cd98098c58f7ac0a7db1715dbabfbbf25f733d
dcoo <djc4ku@virginia.edu>
2020-05-03 22:27:51 -0400

    hyper

--- setup/setup.sh
+++ setup/setup.sh
@@ -14,10 +14,14 @@ code_dir="$HOME/code/"
 ruby_version="2.5.1"
 script_dir="~/.scripts"
 
+# ohmyzsh
+#   git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
+# geometry zsh plugin
 # rbenv
 # ruby
 # homebrew
 # ripgrep
+# cargo for rust
 
 # mac specific
 
